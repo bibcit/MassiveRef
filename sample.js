7239d6f156f17b8bfe3f4f00fbfa478147e2fcd9
Rakesh Kumar <iitd.tanishk@gmail.com>
2026-03-25 20:08:34 +0530

    ok done

--- sample.js
+++ sample.js
@@ -1,4 +1,9 @@
 const axios = require('axios');
+/**
+ * This is a sample code to demonstrate how to make a POST request using Axios in Node.js.
+ * The code sends a JSON payload containing an array of resources to a specified URL.
+ * The response from the server is logged to the console. If there is an error during the request, it is also logged.
+ */
 let data = JSON.stringify({
   "objResource": [
     "https://doi.org/10.1353/arq.1997.0020",
@@ -8,20 +13,26 @@ let data = JSON.stringify({
   ]
 });
 
-let config = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: 'https://www.bibcit.com/api/citation/document',
-  headers: { 
-    'Content-Type': 'application/json'
-  },
-  data : data
-};
 
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
+/**
+ * Still under active development, the URL and other parameters need to be updated before making the actual request.
+ */
+
+
+// let config = {
+//   method: 'post',
+//   maxBodyLength: Infinity,
+//   url: '',
+//   headers: { 
+//     'Content-Type': 'application/json'
+//   },
+//   data : data
+// };
+
+// axios.request(config)
+// .then((response) => {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
